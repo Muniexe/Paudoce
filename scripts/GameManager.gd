41512ac498acb7c0cb0bc981129cce0e7de1d8cd
Eduardo Muniz <edmz112306@gmail.com>
2026-01-05 19:40:55 -0300

    objetivos madeira e sexo

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,3 +1,16 @@
 extends Node
 
-var spawn_position: Vector2
+# Spawn do player ao trocar de cena
+var spawn_position: Vector2 = Vector2.ZERO
+
+
+# ===== SISTEMA DE OBJETIVO =====
+enum QuestState {
+	NONE,
+	GET_WOOD,
+	DELIVER_WOOD,
+	COMPLETED
+}
+
+var quest_state: QuestState = QuestState.GET_WOOD
+var has_wood: bool = false
---
+++ scripts/wood.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+var player_inside := false
+var player_ref
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and GameManager.quest_state == GameManager.QuestState.GET_WOOD:
+		player_inside = true
+		player_ref = body
+		body.show_interact_icon()
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_inside = false
+		player_ref = null
+		body.hide_interact_icon()
+
+func _process(_delta):
+	if player_inside and Input.is_action_just_pressed("interact"):
+		collect_wood()
+
+func collect_wood():
+	GameManager.has_wood = true
+	GameManager.quest_state = GameManager.QuestState.DELIVER_WOOD
+	player_ref.hide_interact_icon()
+	queue_free() # remove a madeira do mapa
