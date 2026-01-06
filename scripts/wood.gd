6fdd3010c2cc3bc486abac6be3889f462e810132
Eduardo Muniz <edmz112306@gmail.com>
2026-01-05 21:00:57 -0300

    Objetivo funcional, inicio do HUD

--- scripts/wood.gd
+++ scripts/wood.gd
@@ -3,11 +3,13 @@ extends Area2D
 var player_inside := false
 var player_ref
 
+
 func _on_body_entered(body):
-	if body.is_in_group("player") and GameManager.quest_state == GameManager.QuestState.GET_WOOD:
+	if body.is_in_group("player") and not GameManager.has_wood:
 		player_inside = true
 		player_ref = body
 		body.show_interact_icon()
+
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
@@ -15,12 +17,13 @@ func _on_body_exited(body):
 		player_ref = null
 		body.hide_interact_icon()
 
+
 func _process(_delta):
 	if player_inside and Input.is_action_just_pressed("interact"):
 		collect_wood()
 
+
 func collect_wood():
 	GameManager.has_wood = true
-	GameManager.quest_state = GameManager.QuestState.DELIVER_WOOD
 	player_ref.hide_interact_icon()
-	queue_free() # remove a madeira do mapa
+	queue_free() # remove ESSA madeira
