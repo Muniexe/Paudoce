e3ad99a762762c92121ac10c39066d8a5b364753
Eduardo Muniz <edmz112306@gmail.com>
2026-01-05 23:52:35 -0300

    Sistema de Missão, HUD e Interação

--- scripts/interact_area.gd
+++ scripts/interact_area.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var target_scene: String = "res://fases/test/house_interior.tscn"
+@export var target_scene: String = "res://fases/interior_da_igreja.tscn"
 
 var player_inside := false
 var player_ref: CharacterBody2D
@@ -10,31 +10,42 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_inside = true
 		player_ref = body
-		print("Player entrou na área")
-		body.show_interact_icon()
+		body.show_interact_icon() # 👈 SEMPRE mostra
 
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		player_inside = false
 		player_ref = null
-		print("Player saiu da área")
 		body.hide_interact_icon()
 
 
 func _process(_delta: float) -> void:
 	if player_inside and Input.is_action_just_pressed("interact"):
-		enter_house()
+		if can_enter():
+			enter_house()
+		else:
+			show_blocked_message()
+
+
+func can_enter() -> bool:
+	return GameManager.wood_delivered >= GameManager.max_wood
+
+
+func show_blocked_message():
+	if GameManager.hud:
+		GameManager.hud.show_tutorial(
+			"Leve todas as madeiras para a igreja"
+		)
 
 
 func enter_house() -> void:
 	$DoorSound.play()
+
 	if player_ref:
 		player_ref.hide_interact_icon()
 
 	Fade.fade_out()
 	await get_tree().create_timer(0.25).timeout
 	get_tree().change_scene_to_file(target_scene)
-
-	# Fade in será chamado automaticamente após a troca
 	Fade.fade_in()
