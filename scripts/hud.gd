a7d92ef8683ed7aa60f36050ee8f28d058d1e02b
Eduardo Muniz <edmz112306@gmail.com>
2026-01-06 13:17:58 -0300

    Hud bom, nova fonte, novos tiles e sexo

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,21 +1,19 @@
 extends CanvasLayer
 
 @onready var wood_label: Label = $Labelwood
-@onready var tutorial_label: Label = $TutorialLabel
+@onready var tutorial_panel: Panel = $TutorialPanel
+@onready var tutorial_label: Label = $TutorialPanel/TutorialLabel
 
 var tutorial_tween: Tween
 
-
 func _ready():
-	# Registra o HUD no GameManager
 	GameManager.hud = self
 
-	# Estado inicial
-	tutorial_label.visible = false
-	tutorial_label.modulate.a = 0.0
+	tutorial_panel.visible = false
+	tutorial_panel.modulate.a = 0.0
 
 	update_wood_label()
-	# 👇 TEXTO INICIAL
+
 	await get_tree().create_timer(0.5).timeout
 	show_tutorial("Pegue as madeiras e leve até a igreja")
 
@@ -33,44 +31,41 @@ func update_wood_label():
 		GameManager.max_wood
 	]
 
-	if GameManager.wood_delivered >= GameManager.max_wood:
-		wood_label.visible = false
-	else:
-		wood_label.visible = true
+	wood_label.visible = GameManager.wood_delivered < GameManager.max_wood
 
 
 # ===============================
-# TEXTO DE OBJETIVO (TUTORIAL)
+# TUTORIAL BOX
 # ===============================
 func show_tutorial(text: String):
 	if tutorial_tween:
 		tutorial_tween.kill()
 
 	tutorial_label.text = text
-	tutorial_label.visible = true
-	tutorial_label.modulate.a = 0.0
+	tutorial_panel.visible = true
+	tutorial_panel.modulate.a = 0.0
 
 	tutorial_tween = create_tween()
 
 	# Fade in
 	tutorial_tween.tween_property(
-		tutorial_label,
+		tutorial_panel,
 		"modulate:a",
 		1.0,
-		0.2
+		0.25
 	)
 
-	# Espera um pouco na tela
-	tutorial_tween.tween_interval(1.8)
+	# Tempo na tela
+	tutorial_tween.tween_interval(2.0)
 
 	# Fade out
 	tutorial_tween.tween_property(
-		tutorial_label,
+		tutorial_panel,
 		"modulate:a",
 		0.0,
-		0.3
+		0.35
 	)
 
 	tutorial_tween.finished.connect(func():
-		tutorial_label.visible = false
+		tutorial_panel.visible = false
 	)
